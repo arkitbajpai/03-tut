844017f5cdcdcd810cf6282deb7245114d01a57a
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-09-01 22:34:36 +0530

    Add search input to Nav component for enhanced search functionality

--- src/Nav.js
+++ src/Nav.js
@@ -5,6 +5,14 @@ const Nav = ({search, setSearch}) => {
     <nav className='Nav'>
        <form className='searchForm' onSubmit={(e)=>e.preventDefault()}>
         <label htmlFor='search'>Search Posts</label>
+        <input
+        id='search'
+        type='text'
+        placeholder='Search Posts'
+        value={search}
+        onChange={(e)=>setSearch(e.target.value)}
+        />
+
        </form>
     </nav>
   )
